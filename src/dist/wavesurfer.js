4fdcf1eadce390a7faea31e83c2d0d0661680e74
Sukolsak Sakshuwong <feedback@pbstanford.org>
2020-07-10 22:05:01 -0700

    (bugfix) fixed waveform rendering when view scrolls

--- src/dist/wavesurfer.js
+++ src/dist/wavesurfer.js
@@ -5831,6 +5831,8 @@ var WebAudio = function (_util$Observer) {
                      //   if (abs_shift > 0.5) {
                         shift = Math.round ( shift );
 
+                        first = this.peaksStart - (shift * sampleSize);
+
                         shift = (shift * 2);
                         //    shift = Math.round ( Math.abs (shift) ) * (shift < 0 ? - 1 : 1);
 
