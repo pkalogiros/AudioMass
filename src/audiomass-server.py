a89d39a5ee9a59fd4eb0ed8bf3dd3079957b8c7b
老阿張 <laoazhang@163.com>
2026-02-27 09:45:42 +0800

    refactor: migrate server script from Python 2 to Python 3

--- src/audiomass-server.py
+++ src/audiomass-server.py
@@ -1,7 +1,9 @@
-import SimpleHTTPServer
-import SocketServer
-PORT = 5055 
-Handler = SimpleHTTPServer.SimpleHTTPRequestHandler
-httpd = SocketServer.TCPServer(("", PORT), Handler)
-print "serving at port", PORT
-httpd.serve_forever()
+import http.server
+import socketserver
+
+PORT = 5055
+Handler = http.server.SimpleHTTPRequestHandler
+
+with socketserver.TCPServer(("", PORT), Handler) as httpd:
+    print("serving at port", PORT)
+    httpd.serve_forever()
